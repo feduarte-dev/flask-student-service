66ccd1f01d1f9021bc7117b32ef67e7c42dbaa49
Felipe Duarte <feduarte.dev@gmail.com>
2024-04-11 01:47:52 -0300

    requisito 2

--- src/models/student_model.py
+++ src/models/student_model.py
@@ -4,3 +4,12 @@ from database.db import db
 
 class StudentModel(AbstractModel):
     _collection = db["students"]
+
+    def __init__(self, data):
+        super().__init__(data)
+
+    def to_dict(self):
+        return {
+            "name": self.data["name"],
+            "enrollment_number": self.data["enrollment_number"],
+        }
